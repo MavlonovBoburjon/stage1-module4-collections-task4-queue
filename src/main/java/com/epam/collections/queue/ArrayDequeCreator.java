00788cee4664e8580f00e99f77270e2441478756
Boburjon Mavlonov <mavlonovboburjon2508@gmail.com>
2026-08-13 16:49:34 +0500

    task completed

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,47 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        // 1. initial (2 tadan)
+        deque.addLast(firstQueue.poll());
+        deque.addLast(firstQueue.poll());
+
+        deque.addLast(secondQueue.poll());
+        deque.addLast(secondQueue.poll());
+
+        // 2. main loop
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+
+            // FIRST PLAYER
+            Integer cardFromDeque1 = deque.pollFirst();
+            if (cardFromDeque1 != null) {
+                firstQueue.add(cardFromDeque1);
+            }
+
+            deque.addLast(firstQueue.poll());
+            deque.addLast(firstQueue.poll());
+
+            // SECOND PLAYER
+            Integer cardFromDeque2 = deque.pollFirst();
+            if (cardFromDeque2 != null) {
+                secondQueue.add(cardFromDeque2);
+            }
+
+            deque.addLast(secondQueue.poll());
+            deque.addLast(secondQueue.poll());
+        }
+
+        // 🔥 MUHIM: qolgan elementlarni ham qo‘shish
+        while (!firstQueue.isEmpty()) {
+            deque.addLast(firstQueue.poll());
+        }
+
+        while (!secondQueue.isEmpty()) {
+            deque.addLast(secondQueue.poll());
+        }
+
+        return deque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,31 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+
+        // fill queue
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+
+            // move k-1 elements
+            for (int i = 1; i < everyDishNumberToEat; i++) {
+                queue.add(queue.poll());
+            }
+
+            // remove k-th
+            result.add(queue.poll());
+        }
+
+        return result;
     }
 }
